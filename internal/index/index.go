7b44509878a0ddeed01148503aba580642155637
Ghazlabs <ghazlabs2020@gmail.com>
2021-01-18 20:14:30 +0300

    finish implement search mechanism on index module; work on progress;

--- internal/index/index.go
+++ internal/index/index.go
@@ -1,9 +1,17 @@
 package index
 
-import "errors"
+import (
+	"errors"
+	"math"
+	"sort"
+)
 
-// ErrDocNotFound represents error returned when document is not found
-var ErrDocNotFound = errors.New("ERR_DOC_NOT_FOUND")
+var (
+	// ErrDocNotFound represents error returned when document is not found
+	ErrDocNotFound = errors.New("ERR_DOC_NOT_FOUND")
+	// ErrPageNotFound represents error returned when page is not found
+	ErrPageNotFound = errors.New("ERR_PAGE_NOT_FOUND")
+)
 
 // Index represents in-memory index for documents
 type Index struct {
@@ -60,16 +68,64 @@ func New(c Configs) (*Index, error) {
 	return &i, nil
 }
 
-// Search returns relevant documents based on given query
+// Search returns relevant documents based on given query, page start from 0
 func (i *Index) Search(q Query, page int) (*SearchResult, error) {
 	// break query into words
+	words := q.GetWords()
 	// create appearance counter map
+	counterMap := map[int]int{}
 	// get document ids for each query words
-	// increment appearance counter map
-	// convert appearance counter map to list
+	for _, word := range words {
+		// if word is excluded word just continue
+		_, excluded := i.excludeWordMap[word]
+		if excluded {
+			continue
+		}
+		// increment appearance counter map
+		docIDs := i.revIndexMap[word]
+		for _, docID := range docIDs {
+			counterMap[docID]++
+		}
+	}
+	// convert appearance counter map to list of tupple (docID, counter)
+	tupples := make([]tuppleDocIDCounter, 0, len(counterMap))
+	for docID, counter := range counterMap {
+		tupples = append(tupples, tuppleDocIDCounter{
+			DocID:   docID,
+			Counter: counter,
+		})
+	}
 	// sort the list from highest to lowest appearance
-	// select the element for given page
-	return nil, nil
+	sort.Slice(tupples, func(i, j int) bool {
+		return tupples[i].Counter < tupples[j].Counter
+	})
+	// check if page is exists
+	totalPages := int(math.Ceil(float64(len(tupples)) / float64(i.pageLimit)))
+	if page >= totalPages {
+		return nil, ErrPageNotFound
+	}
+	// select the elements for given page
+	startIdx := page * i.pageLimit
+	endIdx := startIdx + i.pageLimit
+	if endIdx >= len(tupples) {
+		endIdx = len(tupples)
+	}
+	// convert back tupples to list of docs
+	selectedTupples := tupples[startIdx:endIdx]
+	var docs []Document
+	for _, tupple := range selectedTupples {
+		docs = append(docs, i.docMap[tupple.DocID])
+	}
+	result := &SearchResult{
+		Relevants:  docs,
+		TotalPages: totalPages,
+	}
+	return result, nil
+}
+
+type tuppleDocIDCounter struct {
+	DocID   int
+	Counter int
 }
 
 // Get returns index for given id, throws error if id is not exists
